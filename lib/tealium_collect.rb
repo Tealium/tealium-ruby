a863a9dad0e535cd77bbe02e96629004ef106a58
salf <nikola.symonov@gmail.com>
2021-07-30 19:11:25 +0300

    Fix SSL connectivity

--- lib/tealium_collect.rb
+++ lib/tealium_collect.rb
@@ -4,21 +4,22 @@ require 'json'
 
 class TealiumCollect
   LIBRARY_NAME = "ruby"
-  LIBRARY_VERSION = "1.0.0"
+  LIBRARY_VERSION = "1.0.2"
 
   def self.collect(payload)
     uri = URI.parse("https://collect.tealiumiq.com/event")
     header = {'Content-type': 'application/json'}
-    http = Net::HTTP.new(uri.host)
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
     request = Net::HTTP::Post.new(uri.request_uri, header)
     payload[:tealium_library_name] = LIBRARY_NAME
     payload[:tealium_library_version] = LIBRARY_VERSION
 
-      request.body = payload.to_json
-      response = http.request(request)
+    request.body = payload.to_json
+    response = https.request(request)
 
-      #uncomment the line below for debugging
-      #checkRequestResponse(response)
+    #uncomment the line below for debugging
+    #checkRequestResponse(response)
   end
 
   def checkRequestResponse(response)
